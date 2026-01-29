51d123bfdff33322c1d3a54eb9413a97c647d0b3
Raharison diary <raharisondiary06@gmail.com>
2026-01-29 07:15:46 +0100

    debug

--- server.js
+++ server.js
@@ -3,25 +3,26 @@ const mongoose = require('mongoose');
 const cors = require('cors'); 
 require('dotenv').config();
 
-// 1. Import-eo ny Routes (Ataovy azo antoka fa misy fichiers any amin'ny folder routes)
 const authRoutes = require('./routes/auth'); 
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
 
-const mongoURI = process.env.MONGO_URI || "mongodb+srv://diary:diary@cluster0.q60ysss.mongodb.net/elearning?retryWrites=true&w=majority";
+// Connection DB
+const mongoURI = process.env.MONGO_URI;
 
 mongoose.connect(mongoURI)
   .then(() => console.log("✅ MongoDB Atlas tafiditra!"))
   .catch(err => console.error("❌ Error connection:", err));
 
-// 2. Ampiasao ny routes eto (Ity no nampisy 404 teo)
+// Routes
 app.use('/api/auth', authRoutes);
 
 app.get('/', (req, res) => {
-  res.send("Mandehana tsara ny Backend-ntsika!");
+  res.send("Backend E-learning mandeha tsara!");
 });
 
 const PORT = process.env.PORT || 5000;
